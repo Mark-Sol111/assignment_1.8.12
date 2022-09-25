0a11f63241108ea74bb5a3778d94bd6886db7b7a
Mark Sol <114338588+Mark-Sol111@users.noreply.github.com>
2022-09-26 02:43:06 +1100

    done

--- install.sh
+++ install.sh
@@ -4,7 +4,7 @@ cat "alias l='ls -ln'" > .aliases
 
 #ln -s "/home/box/web/etc/test.conf"  "/etc/nginx/conf.d/test.conf"
 
-sudo ﻿ln -s /home/box/web/etc/test.conf  /etc/nginx/sites-enabled/test.conf
+sudo ln -s /home/box/web/etc/test.conf  /etc/nginx/sites-enabled/test.conf
 sudo rm /etc/nginx/sites-enabled/default
 
 
